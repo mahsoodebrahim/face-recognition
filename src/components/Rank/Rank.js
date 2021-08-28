7395d62ac74bd7e8dd147df76dd492e901937a18
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2021-08-28 10:52:00 -0700

    Updated Rank component to display username and number of user entries

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Rank() {
+function Rank({ userName, userEntries }) {
   return (
     <div>
-      <div className="white f3">Andrei, your image frequency rank is...</div>
-      <div className="white f1">#5</div>
+      <div className="white f3">{userName}, your entry total is...</div>
+      <div className="white f1">{userEntries}</div>
     </div>
   );
 }
